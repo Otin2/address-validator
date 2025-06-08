6ace367769b3d21672230116d533b5088526c212
Rolando Roller <otin@Otins-MacBook-Pro-14.local>
2025-06-08 15:08:45 -0500

    Actualización en el validador de direcciones

--- test_validator.py
+++ test_validator.py
@@ -2,6 +2,7 @@ import unittest
 from validator import is_valid_address
 
 class TestValidator(unittest.TestCase):
+    
     def test_valid_address(self):
         self.assertTrue(is_valid_address("Av. Siempre Viva 742"))
 
